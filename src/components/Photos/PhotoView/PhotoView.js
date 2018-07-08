f5ffaae2a98e66e4a8578d77e6d115e2f056da28
Alex McCabe <alexwmccabe@gmail.com>
2018-07-08 15:31:35 +0100

    Rename: PhotoDetails -> PhotoView

---
+++ src/components/Photos/PhotoView/PhotoView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class PhotoView extends Component {
+    static defaultProps = {};
+    static propTypes = {};
+
+    state = {};
+
+    render() {
+        return <div className="photoview" />;
+    }
+}
