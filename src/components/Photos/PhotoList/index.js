5fb66d3fe35441c91ccaea6bd8cda385e7373916
Alex McCabe <alexwmccabe@gmail.com>
2018-07-08 16:49:45 +0100

    Delete: PhotoListContainer

--- src/components/Photos/PhotoList/index.js
+++ src/components/Photos/PhotoList/index.js
@@ -1,3 +1,2 @@
 export { default as PhotoList } from './PhotoList';
-export { default as PhotoListContainer } from './PhotoListContainer';
 export { default as PhotoItem } from './PhotoItem/PhotoItem';
