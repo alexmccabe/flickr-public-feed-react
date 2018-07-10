53e6a04134699ec7c69c7dbb53a361f9514422b2
Alex McCabe <alexwmccabe@gmail.com>
2018-07-10 08:46:52 +0100

    Add: new component import/export

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as App } from 'components/App/App';
 export { default as Router } from 'components/Router';
+export { default as Search } from 'components/Search/Search';
+export { default as Header } from 'components/Header/Header';
+export { default as SvgIcons } from 'components/SvgIcons/SvgIcons';
 export * from 'components/Photos';
